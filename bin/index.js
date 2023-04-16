b35e032d3bdc134fa931cc50b7005cc3c39e98ad
Anjuman Raj <ayusmann23@gmail.com>
2023-04-17 02:07:26 +0530

    🎨 style(index.js): refactor inquirer prompt to use an array of questions instead of an object ✨ feat(index.js): add more questions to the inquirer prompt to configure the chatbot's GPT model parameters 🐛 fix(index.js): add newline at end of file
    Signed-off-by: Anjuman Raj <ayusmann23@gmail.com>

--- bin/index.js
+++ bin/index.js
@@ -1,26 +1,45 @@
 #!/usr/bin/env node
 import inquirer from 'inquirer';
 
-const questions = [
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
-      type: 'confirm',
-      name: 'confirm',
-      message: 'Are you sure?',
-    },
-    {
-      type: 'list',
-      name: 'fruit',
-      message: 'What is your favorite fruit?',
-      choices: ['Apple', 'Banana', 'Cherry'],
-    },
-  ];
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'model',
+    message: 'What is the name or path of the GPT model file?',
+    default: 'gpt-3.5-turbo',
+  },
+  {
+    type: 'number',
+    name: 'maxTokens',
+    message: 'What is the maximum number of tokens the chatbot should generate in a response?',
+    default: 50,
+  },
+  {
+    type: 'number',
+    name: 'temperature',
+    message: 'What is the temperature parameter for the GPT model?',
+    default: 0.7,
+  },
+  {
+    type: 'number',
+    name: 'topP',
+    message: 'What is the top-p sampling threshold for the GPT model?',
+    default: 0.9,
+  },
+  {
+    type: 'number',
+    name: 'frequencyPenalty',
+    message: 'What is the frequency penalty parameter for the GPT model?',
+    default: 0.0,
+  },
+  {
+    type: 'number',
+    name: 'presencePenalty',
+    message: 'What is the presence penalty parameter for the GPT model?',
+    default: 0.0,
+  },
+])
+.then(answers => {
+  console.log(answers);
+});
 
-  inquirer.prompt(questions).then((answers) => {
-    console.log(answers);
-  });
-  
